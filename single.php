9fa750a32fd1b20881784be36e31841a5f97a3c9
quanticbyte <paucatalan80@gmail.com>
2018-06-12 22:16:49 +0200

    afegida primera versio de comentaris a post i archiu comments.php

--- single.php
+++ single.php
@@ -77,6 +77,41 @@
       </div>
 
 <?php
+//comentaris
+// formulari de comentaris
+// acompanyat de pagina comments.php
+//---------------------------------------------------------------------------------------
+//simple comment loop
+//Get only the approved comments 
+/*
+$args = array(
+    'status' => 'approve'
+);
+ 
+// The comment Query
+$comments_query = new WP_Comment_Query;
+$comments = $comments_query->query( $args );
+ 
+// Comment Loop
+if ( $comments ) {
+    foreach ( $comments as $comment ) {
+        echo '<p>' . $comment->comment_content . '</p>';
+    }
+} else {
+    echo 'No comments found.';
+}
+*/
+//comments template
+// If comments are open or we have at least one comment, load up the comment template.
+ if ( comments_open() || get_comments_number() ) :
+     comments_template();
+ endif;
+
+//---------------------------------------------------------------------------------------
+?>
+
+
+<?php
     endwhile; endif;
 ?>
 
